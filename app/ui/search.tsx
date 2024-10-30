bb553d34f918a5876f259da0329f52f67a967a25
Charlie Allen <charlie06allen@gmail.com>
2024-10-30 19:31:00 +0000

    update search;

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,9 +1,20 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
   function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    const pathname = usePathname();
+    const { replace } = useRouter();
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
     console.log(term);
   }
   return (
