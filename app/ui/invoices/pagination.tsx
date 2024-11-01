1bb9a451eaa08f40eafafee7ebaedbe03af63427
Charlie Allen <charlie06allen@gmail.com>
2024-11-01 09:18:54 +0000

    finish adding pagination component

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -7,9 +7,18 @@ import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
   // NOTE: Uncomment this code in Chapter 11
 
-  const allPages = generatePagination(currentPage, totalPages);
+  // const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
